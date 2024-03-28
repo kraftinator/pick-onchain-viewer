94ef36ad58f4a02f8d2402d086a042afc0fa55b4
Adam Kraft <adamkraft1@gmail.com>
2024-03-28 10:15:57 -0500

    Test 7

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      action: 'link',
+      //action: 'link',
       label: 'Link to Cat',
       target: `${NEXT_PUBLIC_URL}/api/cat`,
     },
