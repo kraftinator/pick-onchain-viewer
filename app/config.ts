1e9342a93638fec2a230c5a4fd31180fe19705c2
Adam Kraft <adamkraft1@gmail.com>
2024-04-02 09:46:42 -0500

    Added vercel url

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
-  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://framinator.vercel.app/';
+  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://pick-onchain-viewer.vercel.app/';
 export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e0832257569c3Fd4AE439dD6f';
 export const PICK_ONCHAIN_CONTRACT_ADDR = '0x2acEbe69Da6532c5b2Ab47FC363B87C802606664';
