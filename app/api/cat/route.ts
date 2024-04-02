9c5b3dc5084bf563bd14441acebe446a59234b0d
Adam Kraft <adamkraft1@gmail.com>
2024-04-02 11:17:42 -0500

    Test 5

--- app/api/cat/route.ts
+++ app/api/cat/route.ts
@@ -12,6 +12,43 @@ import { createPublicClient, http } from 'viem';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
+  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+
+  if (!isValid) {
+    return new NextResponse('Message not valid', { status: 500 });
+  }
+
+  const text = message.input || '';
+
+  console.log('text', text);
+
+  return new NextResponse(
+    getFrameHtmlResponse({
+      buttons: [
+        {
+          label: `Token ID: ${text}`,
+        },
+        {
+          action: 'link',
+          label: 'OnchainKit',
+          target: 'https://onchainkit.xyz',
+        },
+        {
+          action: 'post_redirect',
+          label: 'Dog pictures',
+        },
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/park-1.png`,
+      },
+      postUrl: `${NEXT_PUBLIC_URL}/api/cat`,
+    }),
+  );
+}
+
+/*
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  const body: FrameRequest = await req.json();
 
   let tokenId: bigint = 371n;
 
@@ -37,57 +74,10 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
   return NextResponse.json(picks);
 
-  /*
-  const data = encodeFunctionData({
-    abi: PickOnchainABI,
-    functionName: 'ViewPickByTokenID',
-    args: [tokenId],
-  });
-
-  const txData: FrameTransactionResponse = {
-    chainId: `eip155:${base.id}`,
-    method: 'eth_sendTransaction',
-    params: {
-      abi: [],
-      data,
-      to: PICK_ONCHAIN_CONTRACT_ADDR,
-      //value: '0',
-      value: parseEther('0.00004').toString(), // 0.00004 ETH
-    },
-  };
-  console.log('Hello, world!')
-  console.log('txData', txData)
-  return NextResponse.json(txData);
-  */
-
-  /*
-  return new NextResponse(
-    getFrameHtmlResponse({
-      buttons: [
-        {
-          label: 'The Cat Page',
-        },
-        {
-          action: 'link',
-          label: 'OnchainKit',
-          target: 'https://onchainkit.xyz',
-        },
-        {
-          action: 'post_redirect',
-          label: 'Dog pictures',
-        },
-      ],
-      image: {
-        src: `${NEXT_PUBLIC_URL}/cat1.jpg`,
-      },
-    }),
-  );
-  */
 }
+*/
 
 export async function POST(req: NextRequest): Promise<Response> {
-  //console.log('FLAG 3');
-  //console.log('req', req);
   return getResponse(req);
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ const frameMetadata = getFrameMetadata({
     text: 'Enter Token ID',
   },
   image: {
-    src: `${NEXT_PUBLIC_URL}/spaghetti.jpg`,
-    aspectRatio: '1:1',
+    src: `${NEXT_PUBLIC_URL}/spaghetti1.jpg`,
+    aspectRatio: '1:1',    
   },
   buttons: [
     {
