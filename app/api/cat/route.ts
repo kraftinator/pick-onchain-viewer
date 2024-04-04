d6a0e0b216d43552a8cd861685f5a831fec0867c
Adam Kraft <adamkraft1@gmail.com>
2024-04-04 14:53:03 -0500

    Test 64

--- app/api/cat/route.ts
+++ app/api/cat/route.ts
@@ -609,7 +609,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         },
         {
           label: 'START OVER',
-          target: `${NEXT_PUBLIC_URL}/api/mfer`,
+          target: `${NEXT_PUBLIC_URL}/api/frame`,
         },
       ],
       image: {
