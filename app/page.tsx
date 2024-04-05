1dd74bd20cf75d08998df733fc421ae994a11ae9
Adam Kraft <adamkraft1@gmail.com>
2024-04-05 11:22:04 -0500

    Test 88

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   image: {
     src: `${NEXT_PUBLIC_URL}/pick-onchain-main.png`,
-    aspectRatio: '1:1',    
+    //aspectRatio: '1:1',    
   },
   buttons: [
     {
