a41c23a573155b425805e9bb99fc66fbfb1bb3a7
Adam Kraft <adamkraft1@gmail.com>
2024-04-05 12:22:49 -0500

    Test 102

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ const frameMetadata = getFrameMetadata({
 
   image: {
     src: `${NEXT_PUBLIC_URL}/pick-onchain-main3.png`,
-    //aspectRatio: '1:1',    
   },
   buttons: [
     {
@@ -16,76 +15,13 @@ const frameMetadata = getFrameMetadata({
   ]
 });
 
-/*
-const frameMetadata = getFrameMetadata({
-  input: {
-    text: 'Enter Token ID',
-  },
-  image: {
-    src: `${NEXT_PUBLIC_URL}/spaghetti.jpg`,
-    aspectRatio: '1:1',    
-  },
-  buttons: [
-    {
-      label: 'View Bracket',
-      target: `${NEXT_PUBLIC_URL}/api/cat`,
-    },
-  ]
-});
-*/
-
-/*
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      label: 'Link to Cat',
-      target: `${NEXT_PUBLIC_URL}/api/cat`,
-      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-    },
-    {
-      label: 'Link to mfer',
-      target: `${NEXT_PUBLIC_URL}/api/mfer`,
-    },
-  ],
-  image: {
-    src: `${NEXT_PUBLIC_URL}/spaghetti.jpg`,
-    aspectRatio: '1:1',
-  },
-  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-});
-*/
-
-/*
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      label: 'Story time',
-    },
-    {
-      action: 'tx',
-      label: 'Send Base Sepolia',
-      target: `${NEXT_PUBLIC_URL}/api/tx`,
-      postUrl: `${NEXT_PUBLIC_URL}/api/tx-success`,
-    },
-  ],
-  image: {
-    src: `${NEXT_PUBLIC_URL}/park-3.png`,
-    aspectRatio: '1:1',
-  },
-  input: {
-    text: 'Tell me a story',
-  },
-  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-});
-*/
-
 export const metadata: Metadata = {
-  title: 'zizzamia.xyz',
-  description: 'LFG',
+  title: 'Bracket Viewer',
+  description: 'An unofficial Bracket Viewer for the Polymarket Madness competion',
   openGraph: {
-    title: 'zizzamia.xyz',
-    description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+    title: 'Bracket Viewer',
+    description: 'An unofficial Bracket Viewer for the Polymarket Madness competion',
+    //images: [`${NEXT_PUBLIC_URL}/park-1.png`],
   },
   other: {
     ...frameMetadata,
@@ -95,7 +31,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <h1>kraftinator</h1>
+      <h1>Sorry, the Bracket Viewer can only be viewed in a Frame.</h1>
     </>
   );
 }
